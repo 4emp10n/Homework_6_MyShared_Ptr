54835253b3f5322312b6a4f98cbf415ec9a715d1
Aleksandr Dziuba <dziubaaleksandr22@gmail.com>
2021-10-15 21:29:43 +0000

    Add copy assignment

--- MyShared_ptr.h
+++ MyShared_ptr.h
@@ -7,8 +7,9 @@ class MyShared_ptr
     public:
     MyShared_ptr(): m_ptr(nullptr), m_ptrCounter(new int(1)){}
     MyShared_ptr(T* ptr = nullptr): m_ptr(ptr), m_ptrCounter(new int (1)){}
-
     MyShared_ptr(const MyShared_ptr& ptr);
+
+    MyShared_ptr& operator=(MyShared_ptr & ptr);
 
 };
 
@@ -19,6 +20,25 @@ MyShared_ptr<T>::MyShared_ptr(const MyShared_ptr& ptr)
     m_ptrCounter = ptr.m_ptrCounter;
 
     if(ptr.m_ptrCounter != nullptr)
+    {
+        (*m_ptrCounter)++;
+    }
+}
+
+template <class T>
+MyShared_ptr<T>& MyShared_ptr<T>::operator=(MyShared_ptr<T> & ptr) // copy assignment
+{
+    (*m_ptrCounter)--;
+		if (*m_ptrCounter == 0)
+		{
+			if (m_ptr != nullptr) 
+				delete m_ptr;
+			delete m_ptrCounter;
+		}
+    m_ptr = ptr.m_ptr;
+    m_ptrCounter = ptr.m_ptrCounter;
+    
+    if (ptr.m_ptrCounter != nullptr)
     {
         (*m_ptrCounter)++;
     }
