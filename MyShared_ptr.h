0cd3029b333f2dec99549e92c23834851f918499
Aleksandr Dziuba <dziubaaleksandr22@gmail.com>
2021-10-15 20:50:39 +0000

    Add constructors

--- MyShared_ptr.h
+++ MyShared_ptr.h
@@ -2,9 +2,24 @@ template<class T>
 class MyShared_ptr
 {
     private:
-    T* ptr = nullptr;
-    int* ptrCounter = nullptr;
+    T* m_ptr = nullptr;
+    int* m_ptrCounter = nullptr;
     public:
-    MyShared_ptr(): ptr(nullptr), ptrCounter(new int(1)){}
-    MyShared_ptr(T* ptr = nullptr): ptr(ptr), ptrCounter(new int (1)){}
+    MyShared_ptr(): m_ptr(nullptr), m_ptrCounter(new int(1)){}
+    MyShared_ptr(T* ptr = nullptr): m_ptr(ptr), m_ptrCounter(new int (1)){}
+
+    MyShared_ptr(const MyShared_ptr& ptr);
+
 };
+
+template<class T>
+MyShared_ptr<T>::MyShared_ptr(const MyShared_ptr& ptr)
+{
+    m_ptr = ptr;
+    m_ptrCounter = ptr.m_ptrCounter;
+
+    if(ptr.m_ptrCounter != nullptr)
+    {
+        (*m_ptrCounter)++;
+    }
+}
