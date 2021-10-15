1329c50603ec3f024db075e3de0591475d407f6f
Aleksandr Dziuba <dziubaaleksandr22@gmail.com>
2021-10-15 20:50:15 +0000

    Add constructors

--- MyShared_ptr.h
+++ MyShared_ptr.h
@@ -1,5 +1,10 @@
 template<class T>
 class MyShared_ptr
 {
-    
+    private:
+    T* ptr = nullptr;
+    int* ptrCounter = nullptr;
+    public:
+    MyShared_ptr(): ptr(nullptr), ptrCounter(new int(1)){}
+    MyShared_ptr(T* ptr = nullptr): ptr(ptr), ptrCounter(new int (1)){}
 };
